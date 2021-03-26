7a8d3f28e7b5a0aa7ea97de83f6303673a44af30
Emine YENİCE <emine_yenice@hotmail.com>
2021-03-26 13:56:53 +0300

    notes controller and ui

--- EasyNote/Models/Note.cs
+++ EasyNote/Models/Note.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using EasyNote.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
@@ -16,8 +17,19 @@ namespace EasyNote.Models
         public string Title { get; set; }
         public string Content { get; set; }
 
+        [Required]
         [ForeignKey("Author")]
         public string AuthorId { get; set; }
         public virtual ApplicationUser Author { get; set; }
+
+        public NoteVM ToViewModel()
+        {
+            return new NoteVM()
+            {
+                Id = Id,
+                Title = Title,
+                Content = Content
+            };
+        }
     }
 }
